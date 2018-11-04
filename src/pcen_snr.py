8a460082c6fdab6346fec23c6d4ad7a371c9577c
Vincent Lostanlen <vincent.lostanlen@nyu.edu>
2018-11-04 08:40:08 -0500

    convert medfilt_time_constant to kernel_size

--- src/pcen_snr.py
+++ src/pcen_snr.py
@@ -4,14 +4,16 @@ import scipy
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
+
+
 def detect_activity(y, sr,
         n_mels=128, fmin=1000, fmax=11025, 
         hop_length=512, gain=0.8, bias=10, power=0.25, pcen_time_constant=0.06, eps=1e-06,
         medfilt_time_constant=None, normalized=True,
         peak_threshold=0.45, activity_threshold=0.2):
     """
-    This function detect the voice activities in signal y and return the start and
-    end time of each event. 
+    This function detects the segments of sound activity in a waveform and returns the start and
+    end time of each sound event. 
     
     Parameters
     ----------
@@ -21,13 +23,13 @@ def detect_activity(y, sr,
     sr : number > 0 [scalar]
         The audio sampling rate
         
-    n_mels:int > 0 [scalar]
+    n_mels : int > 0 [scalar]
         number of Mel bands to generate
 
-    fmin:float >= 0 [scalar]
+    fmin : float >= 0 [scalar]
         lowest frequency (in Hz)
 
-    fmax:float >= 0 [scalar]
+    fmax : float >= 0 [scalar]
         highest frequency (in Hz). If None, use fmax = sr / 2.0
 
     hop_length : int > 0 [scalar]
@@ -56,27 +58,28 @@ def detect_activity(y, sr,
         Whether to normalize the pcen_snr output to between 0 and 1.
         
     peak_threshold : float >= 0
-        The threshold for find_peaks function.
+        The threshold for peak-picking function.
         
     activity_threshold : float >= 0
-        The threshold for defining the start and end of a voice activity.
-
+        The threshold for defining the start and end timestamps of segments.
+        
     Returns
     -------
-    start_times : np.ndarray, non-negative 
+    start_times : np.ndarray, nonnegative 
         The start times of voice activities detected in signal y (seconds).
         
-    end_times : np.ndarray, non-negative 
+    end_times : np.ndarray, nonnegative 
         The end times of voice activities detected in signal y (seconds).
         
     """
-    # 1. compute mel-frequency spectrogram
-    melspec = librosa.feature.melspectrogram(y, sr=sr, fmin=fmin, fmax=fmax, n_mels=n_mels)
+    # 1. Compute mel-frequency spectrogram
+    melspec = librosa.feature.melspectrogram(
+        y, sr=sr, fmin=fmin, fmax=fmax, n_mels=n_mels)
     logmelspec = librosa.power_to_db(melspec)
     
-    # 2. compute PCEN
-    pcen = librosa.core.pcen(melspec, sr=sr,
-    gain=gain, bias=bias, power=power, time_constant=pcen_time_constant, eps=eps)
+    # 2. Compute per-channel energy normalization (PCEN-SNR)
+    pcen = librosa.core.pcen(melspec, sr=sr, gain=gain, bias=bias,
+        power=power, time_constant=pcen_time_constant, eps=eps)
     
     # 3. compute PCEN-SNR detection function
     pcen_snr = np.max(pcen,axis=0) - np.min(pcen,axis=0)
@@ -84,23 +87,23 @@ def detect_activity(y, sr,
     if normalized:
         pcen_snr = pcen_snr / np.max(pcen_snr)
         
-    if medfilt_time_constant is None:
-        medfilt_time_constant = 1
-    else:
-        medfilt_time_constant = medfilt_time_constant * sr
-        
-    median_pcen_snr = scipy.signal.medfilt(pcen_snr, kernel_size=medfilt_time_constant)
+    # 4. Apply median filtering.
+    if medfilt_time_constant is not None:
+        kernel_size = 1 + 2 * round(medfilt_time_constant * sr - 0.5)
+        pcen_snr = scipy.signal.medfilt(pcen_snr, kernel_size=kernel_size)
     
-    # 4. extract active segments
-    activity, start, end = vad(median_pcen_snr,peak_threshold,activity_threshold)
+    # 5. Extract active segments.
+    activity, start, end = threshold_activity(
+        pcen_snr, peak_threshold, activity_threshold)
     
-    # 5. convert indices to seconds
+    # 6. Convert indices to seconds.
     start_times = np.round(np.array(start) * hop_length / sr, 3)
     end_times = np.round(np.array(end) * hop_length / sr, 3)
     
     return start_times, end_times
 
-def vad(x,Tp,Ta):
+
+def threshold_activity(x, Tp, Ta):
     locs = scipy.signal.find_peaks(x,height = Tp)[0]
     y = (x > Ta) * 1
     act = np.diff(y)
@@ -109,10 +112,10 @@ def vad(x,Tp,Ta):
     signal_length = len(x)
     
     if d[0] < u[0]:
-        u = np.insert(u,0,0)
+        u = np.insert(u, 0, 0)
         
     if d[-1] < u[-1]:
-        d = np.append(d,signal_length-1)
+        d = np.append(d, signal_length-1)
         
     starts = []
     ends = []
